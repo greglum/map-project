3cf449f6b73e6a3f1e0372037154776843dea2fb
Greg Lum <greg.lum@pm.me>
2025-04-18 11:41:56 -1000

    Add boilerplate SwiftUI App to migrate

---
+++ AhupuaaApp/AhupuaaApp/AhupuaaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AhupuaaAppApp.swift
+//  AhupuaaApp
+//
+//  Created by Greg  on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AhupuaaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AhupuaaApp/AhupuaaAppTests/AhupuaaAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AhupuaaAppTests.swift
+//  AhupuaaAppTests
+//
+//  Created by Greg  on 4/18/25.
+//
+
+import Testing
+@testable import AhupuaaApp
+
+struct AhupuaaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
